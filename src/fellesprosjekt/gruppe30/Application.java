0e0cf652beb0a138fd98c99eac9a83f663219687
Emil <Emil@78.91.37.242>
2014-03-05 11:49:39 +0100

    Edited message

--- src/fellesprosjekt/gruppe30/Application.java
+++ src/fellesprosjekt/gruppe30/Application.java
@@ -3,6 +3,6 @@ package fellesprosjekt.gruppe30;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Fuck you!");
     }
 }
