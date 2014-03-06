d20104d9da4d114c760444eba6c3c560a781d12f
Anders Wenhaug <andersw@broadpark.no>
2014-03-06 23:46:49 +0100

    Added id, removed room_nmr (they're practically the same thing, but id is used in other models).

--- src/fellesprosjekt/gruppe30/Model/MeetingRoom.java
+++ src/fellesprosjekt/gruppe30/Model/MeetingRoom.java
@@ -1,16 +1,22 @@
 package fellesprosjekt.gruppe30.Model;
 
 public class MeetingRoom {
+    private int id;
 	int room_size;
-	int room_nmr;
 	
-	public MeetingRoom(int room_size, int room_nmr) {
+	public MeetingRoom(int room_size) {
 		this.room_size = room_size;
-		this.room_nmr = room_nmr;	}
-	
+    }
+
+    public int get_id() {
+        return id;
+    }
+
+    public void set_id(int id) {
+        this.id = id;
+    }
+
 	public int get_room_size() {
-		return room_size;	}
-	
-	public int get_room_nmr() {
-		return room_nmr;	}
+		return room_size;
+    }
 }
