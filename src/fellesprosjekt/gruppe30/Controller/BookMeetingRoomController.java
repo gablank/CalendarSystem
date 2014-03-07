294f2654f5ddd1e6e96a130b2df908175f529796
Anders <wenhaug@stud.ntnu.no>
2014-03-07 17:21:43 +0100

    Commented out a line to avoid errors.

--- src/fellesprosjekt/gruppe30/Controller/BookMeetingRoomController.java
+++ src/fellesprosjekt/gruppe30/Controller/BookMeetingRoomController.java
@@ -2,7 +2,7 @@ package fellesprosjekt.gruppe30.Controller;
 
 public class BookMeetingRoomController {
 	
-	public void 
+	//public void
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
