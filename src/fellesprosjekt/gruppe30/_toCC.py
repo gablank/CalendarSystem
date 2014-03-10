8d8a72e97a8a552785658cb48f6fb24041bbfb5b
Anders <wenhaug@stud.ntnu.no>
2014-03-10 14:10:04 +0100

    Committing the _toCC script on EmilJS' request

---
+++ src/fellesprosjekt/gruppe30/_toCC.py
@@ -0,0 +1,38 @@
+__author__ = 'anders'
+
+import os
+import os.path
+
+
+dirtocheck = os.getcwd()
+
+for root, _, files in os.walk(dirtocheck):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        extension = fullpath.split('.')
+
+        if extension[-1] == 'java':
+            print(fullpath)
+            f = open(fullpath, 'r')
+            lines = []
+            for line in f:
+                newLine = ""
+                upperNext = False
+                for i in range(len(line)):
+                    if line[i] == '_' and i + 1 < len(line) and line[i+1].lower() == line[i+1]:
+                        upperNext = True
+                    else:
+                        if upperNext:
+                            newLine += line[i].upper()
+                            upperNext = False
+                        else:
+                            newLine += line[i]
+                lines.append(newLine)
+            f.close()
+            f = open(fullpath, 'w')
+            for line in lines:
+                f.write(line)
+            f.close()
+
+
+
