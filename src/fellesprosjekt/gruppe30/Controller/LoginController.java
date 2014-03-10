aa7df8b732e4755c084aad46de024933bd27c725
Emil <Emil@78.91.37.254>
2014-03-10 13:24:37 +0100

    Did something

--- src/fellesprosjekt/gruppe30/Controller/LoginController.java
+++ src/fellesprosjekt/gruppe30/Controller/LoginController.java
@@ -9,15 +9,30 @@ public class LoginController {
 		this.client = client;
 	}
 	
-	public void get_credentials() {
-		String username = client.loginView.get_username();
-		String password = client.loginView.get_password();
+	public boolean check_credentials() {
+		String username = client.get_login_view().get_username();
+		String password = client.get_login_view().get_password();
 		if (is_valid(username, password)) {
-			System.out.println("YAY");
-		}
+			/*
+			 * Set user information
+			 */
+			return true;
+		} return false;
 	}
 	
 	private boolean is_valid(String username, String password) {
 		return true;
+	}
+	
+	public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
+        String cmd = actionEvent.getActionCommand();
+        
+        if (cmd.equalsIgnoreCase("login")) {
+        	if (check_credentials()) {
+        		client.open("CalendarView");
+        	} else client.get_login_view().view_notifier(); // Shows wrong username/password notifier
+        } else if (cmd.equalsIgnoreCase("quit")) {
+        	
+        }
 	}
 }
