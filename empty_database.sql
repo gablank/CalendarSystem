92fc74a86b88b681a149f3b80feae64fb66bd727
Anders <wenhaug@stud.ntnu.no>
2014-03-12 17:23:28 +0100

    Added script for emptying the database (for testing purposes)

---
+++ empty_database.sql
@@ -0,0 +1,14 @@
+USE calendar_system;
+
+DELETE FROM alarms;
+DELETE FROM meeting_room_reservations;
+DELETE FROM group_members;
+DELETE FROM appointments;
+DELETE FROM attendants;
+DELETE FROM users;
+DELETE FROM external_attendants;
+DELETE FROM internal_attendants;
+DELETE FROM external_users;
+DELETE FROM internal_users;
+DELETE FROM groups;
+DELETE FROM meeting_rooms;
