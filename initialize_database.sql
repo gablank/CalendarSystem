bb01523c28bbbaad93fe45e6ac4639614dd6c181
Anders <wenhaug@stud.ntnu.no>
2014-03-12 12:36:14 +0100

    Updated database creation script to fit the new requirements.

--- initialize_database.sql
+++ initialize_database.sql
@@ -3,16 +3,24 @@ CREATE DATABASE IF NOT EXISTS calendar_system;
 USE calendar_system;
 
 CREATE TABLE IF NOT EXISTS users (
-  id         INT      NOT NULL AUTO_INCREMENT,
-  username   CHAR(20) NOT NULL,
-  password   CHAR(64) NOT NULL,  # sha256
+  email CHAR(50) NOT NULL,
+
+  PRIMARY KEY(email)
+);
+
+CREATE TABLE IF NOT EXISTS internal_users (
+  email      CHAR(50) NOT NULL,
+  password   CHAR(64) NOT NULL,  # sha256 = 64 chars
   first_name CHAR(50) NOT NULL,
   last_name  CHAR(50) NOT NULL,
-  email      CHAR(50) NOT NULL,
 
-  UNIQUE (username),
-  UNIQUE (email),
-  PRIMARY KEY (id)
+  PRIMARY KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS external_users (
+  email CHAR(50) NOT NULL,
+
+  PRIMARY KEY(email)
 );
 
 
@@ -26,16 +34,16 @@ CREATE TABLE IF NOT EXISTS meeting_rooms (
 
 CREATE TABLE IF NOT EXISTS appointments (
   id           INT       NOT NULL AUTO_INCREMENT,
-  name         CHAR(100) NOT NULL,
+  title        CHAR(100) NOT NULL,
   description  CHAR(200) NOT NULL,
   start_date   DATETIME  NOT NULL,
   end_date     DATETIME  NOT NULL,
   place        CHAR(50)  NULL,
   last_updated DATETIME  NOT NULL,
-  owner_id     INT       NOT NULL,
+  owner_email  CHAR(50)  NOT NULL,
 
-  FOREIGN KEY (owner_id)
-    REFERENCES users(id)
+  FOREIGN KEY (owner_email)
+    REFERENCES users(email)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
 
@@ -45,7 +53,7 @@ CREATE TABLE IF NOT EXISTS appointments (
 
 CREATE TABLE IF NOT EXISTS alarms (
   appointment_id INT      NOT NULL,
-  user_id        INT      NOT NULL,
+  user_email     CHAR(50) NOT NULL,
   time           DATETIME NOT NULL,
 
   FOREIGN KEY (appointment_id)
@@ -53,12 +61,12 @@ CREATE TABLE IF NOT EXISTS alarms (
     ON UPDATE CASCADE
     ON DELETE CASCADE,
 
-  FOREIGN KEY (user_id)
-    REFERENCES users(id)
+  FOREIGN KEY (user_email)
+    REFERENCES users(email)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
 
-  PRIMARY KEY (appointment_id, user_id)
+  PRIMARY KEY (appointment_id, user_email)
 );
 
 
@@ -80,15 +88,13 @@ CREATE TABLE IF NOT EXISTS meeting_room_reservations (
 );
 
 
-CREATE TABLE IF NOT EXISTS user_appointments (
-  user_id        INT      NOT NULL,
+CREATE TABLE IF NOT EXISTS attendants (
+  user_email     CHAR(50) NOT NULL,
   appointment_id INT      NOT NULL,
   status         INT      NOT NULL,
-  last_checked   DATETIME NOT NULL,
-  is_visible     BOOL     NOT NULL,
 
-  FOREIGN KEY (user_id)
-    REFERENCES users(id)
+  FOREIGN KEY (user_email)
+    REFERENCES users(email)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
 
@@ -97,5 +103,67 @@ CREATE TABLE IF NOT EXISTS user_appointments (
     ON UPDATE CASCADE
     ON DELETE CASCADE,
 
-  PRIMARY KEY (user_id, appointment_id)
+  PRIMARY KEY (user_email, appointment_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS internal_attendants (
+  user_email     CHAR(50) NOT NULL,
+  appointment_id INT      NOT NULL,
+  last_checked   DATETIME NOT NULL,
+  is_visible     BOOL     NOT NULL,
+
+  FOREIGN KEY (user_email)
+    REFERENCES users(email)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (appointment_id)
+    REFERENCES appointments(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  PRIMARY KEY (user_email, appointment_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS external_attendants (
+  user_email     CHAR(50) NOT NULL,
+  appointment_id INT      NOT NULL,
+
+  FOREIGN KEY (user_email)
+    REFERENCES users(email)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (appointment_id)
+    REFERENCES appointments(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  PRIMARY KEY (user_email, appointment_id)
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  id   INT      NOT NULL AUTO_INCREMENT,
+  name CHAR(50) NOT NULL,
+
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+  user_email CHAR(50) NOT NULL,
+  group_id   INT      NOT NULL,
+
+  FOREIGN KEY (user_email)
+    REFERENCES users(email)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (group_id)
+    REFERENCES groups(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  PRIMARY KEY(user_email, group_id)
 );
