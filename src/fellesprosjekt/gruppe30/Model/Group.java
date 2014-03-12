7af96b5666025061ccf2ac86ef4bca31b82eb5c0
Anders <wenhaug@stud.ntnu.no>
2014-03-12 16:27:06 +0100

    Added name field with getter and setter, and added getJSON

--- src/fellesprosjekt/gruppe30/Model/Group.java
+++ src/fellesprosjekt/gruppe30/Model/Group.java
@@ -1,20 +1,35 @@
 package fellesprosjekt.gruppe30.Model;
 
 
+import org.json.JSONObject;
+
 import java.util.List;
 
 public class Group {
 	private List<User> members;
+	private String name;
 
-	public Group() {
 
+	public Group(String name) {
+		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	public void addMember(User newMember) {
 		this.members.add(newMember);
 	}
 
 	public List<User> getMembers() {
 		return this.members;
+	}
+
+	public JSONObject getJSON() {
+		return null;
 	}
 }
