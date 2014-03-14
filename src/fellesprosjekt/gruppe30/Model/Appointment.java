6ba3cc9707339eadbef7bb9788cee25eeffd6b49
Anders <wenhaug@stud.ntnu.no>
2014-03-14 16:53:27 +0100

    Fixed getDayOfWeek so monday = 0 and sunday = 6

--- src/fellesprosjekt/gruppe30/Model/Appointment.java
+++ src/fellesprosjekt/gruppe30/Model/Appointment.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.*;
+import java.util.Calendar;
 
 public class Appointment {
 	private int             id;
@@ -205,6 +206,11 @@ public class Appointment {
 	public int getDayOfWeek() {
 		GregorianCalendar cal = new GregorianCalendar();
 		cal.setTime(start);
-		return cal.get(java.util.Calendar.DAY_OF_WEEK);
+		int dayOfWeek = cal.get(java.util.Calendar.DAY_OF_WEEK);
+		dayOfWeek -= 1;
+		if(dayOfWeek == 0) {
+			dayOfWeek = 6;
+		}
+		return dayOfWeek;
 	}
 }
